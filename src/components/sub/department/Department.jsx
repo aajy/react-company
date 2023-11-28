49d0d061700c7a8c38fdb517ddfb2f5585081f37
aajy <aajy.000@gmail.com>
2023-11-28 23:59:17 +0900

    Department작업 시작

--- src/components/sub/department/Department.jsx
+++ src/components/sub/department/Department.jsx
@@ -1,5 +1,13 @@
 import './Department.scss';
 
 export default function Department() {
-	return <div className='Department'>Department</div>;
+	return (
+		<div className='Department'>
+			<div className='wrap'>
+				<h2>department</h2>
+				<article className='department-top'></article>
+				<article className='department-bottom'></article>
+			</div>
+		</div>
+	);
 }

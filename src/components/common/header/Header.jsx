540d97890ef90798da33b8a0a0d94edb712b6c37
aajy <aajy.000@gmail.com>
2023-11-29 23:54:36 +0900

    Header.jsx dropBox추가

--- src/components/common/header/Header.jsx
+++ src/components/common/header/Header.jsx
@@ -1,18 +1,58 @@
+import { useState } from 'react';
 import './Header.scss';
 
 export default function Header() {
+	const [Global, setGlobal] = useState('GLOBAL IN');
+	const [IsGlobalDropBoxOpen, setIsGlobalDropBoxOpen] = useState(false);
+	const globalList = [
+		'GLOBAL IN',
+		'Africa',
+		'Asia Pacific',
+		'Europe',
+		'Latin America',
+	];
+	const handleBlurGlobalContainer = () => {
+		setTimeout(() => {
+			setIsGlobalDropBoxOpen(!IsGlobalDropBoxOpen);
+		}, 200);
+	};
+	const handleGlobalOpen = () => {
+		setIsGlobalDropBoxOpen(!IsGlobalDropBoxOpen);
+	};
+	const handleGlobal = (e, el) => {
+		const newGlobal = el;
+		setGlobal(newGlobal);
+	};
 	return (
 		<div className='Header'>
 			<div className='top'>
 				<h1>Abbvie</h1>
 				<div className='dropBox'>
-					<ul>
-						<li>GLOBAL IN</li>
-						<li>Africa</li>
-						<li>Asia Pacific</li>
-						<li>Europe</li>
-						<li>Latin America</li>
-					</ul>
+					<div
+						className='container'
+						onBlur={(e) => handleBlurGlobalContainer(e)}
+					>
+						<label onClick={() => handleGlobalOpen()}>
+							<button className='dropdown-button'>{Global}</button>
+						</label>
+						{IsGlobalDropBoxOpen && (
+							<div
+								className={
+									IsGlobalDropBoxOpen
+										? 'dropdown-container open'
+										: 'dropdown-container'
+								}
+							>
+								<ul className='dropdown-content'>
+									{globalList.map((el, idx) => (
+										<li key={idx} onClick={(e) => handleGlobal(e, el)}>
+											{el}
+										</li>
+									))}
+								</ul>
+							</div>
+						)}
+					</div>
 				</div>
 			</div>
 			<div className='bottom'>

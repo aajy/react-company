e89acc8ed946e7ce5a0ee082d0da83fa4b958074
aajy <aajy.000@gmail.com>
2023-11-29 23:43:51 +0900

    Header.jsx jsx추가

--- src/components/common/header/Header.jsx
+++ src/components/common/header/Header.jsx
@@ -3,8 +3,33 @@ import './Header.scss';
 export default function Header() {
 	return (
 		<div className='Header'>
-			<div className='top'></div>
-			<div className='bottom'></div>
+			<div className='top'>
+				<h1>Abbvie</h1>
+				<div className='dropBox'>
+					<ul>
+						<li>GLOBAL IN</li>
+						<li>Africa</li>
+						<li>Asia Pacific</li>
+						<li>Europe</li>
+						<li>Latin America</li>
+					</ul>
+				</div>
+			</div>
+			<div className='bottom'>
+				<ul>
+					<li>INVESTORS</li>
+					<li>NEWSCENTER</li>
+					<li>PATIENT ASSISTANCE</li>
+				</ul>
+				<ul>
+					<li>Department</li>
+					<li>Youtube</li>
+					<li>Gallery</li>
+					<li>Community</li>
+					<li>Contact</li>
+					<li>search</li>
+				</ul>
+			</div>
 		</div>
 	);
 }

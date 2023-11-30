e61524dd80fed7abfe1d2680aa2b6c5f4815d0ae
aajy <aajy.000@gmail.com>
2023-11-30 23:00:43 +0900

    Layout 컴포넌트 마운트 시 useRef를 통해 class on 붙여주기

--- src/components/common/layout/Layout.jsx
+++ src/components/common/layout/Layout.jsx
@@ -1,5 +1,18 @@
+import { useEffect, useRef } from 'react';
 import './Layout.scss';
 
-export default function Layout() {
-	return <div className='Layout'>Layout</div>;
+export default function Layout({ title, children }) {
+	const refFrame = useRef(null);
+
+	useEffect(() => {
+		setTimeout(() => {
+			refFrame.current.classList.add('on');
+		}, 300);
+	}, []);
+	return (
+		<main className={`Layout ${title}`} ref={refFrame}>
+			<h1>{title}</h1>
+			{children}
+		</main>
+	);
 }
--- src/components/sub/department/Department.jsx
+++ src/components/sub/department/Department.jsx
@@ -1,13 +1,13 @@
+import Layout from '../../common/layout/Layout';
 import './Department.scss';
 
 export default function Department() {
 	return (
-		<div className='Department'>
+		<Layout title='Department'>
 			<div className='wrap'>
-				<h2>department</h2>
 				<article className='department-top'></article>
 				<article className='department-bottom'></article>
 			</div>
-		</div>
+		</Layout>
 	);
 }

5f8cd9084b52c80e5f8db326fc18ad81622f5acb
aajy <aajy.000@gmail.com>
2023-12-02 23:43:40 +0900

    Department jsx작업

--- src/components/sub/department/Department.jsx
+++ src/components/sub/department/Department.jsx
@@ -4,7 +4,80 @@ import './Department.scss';
 export default function Department() {
 	return (
 		<Layout title={'Department-introduction'} className={'Department'}>
-			<article className='department-top'></article>
+			<article className='department-top'>
+				<ul>
+					<li>img1</li>
+					<li>img2</li>
+					<li>
+						<h3>SAGE</h3>
+						<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque, quidem. Laudantium, autem?</p>
+						<button>More</button>
+					</li>
+				</ul>
+				<ul>
+					<li>img1</li>
+					<li>img2</li>
+					<li>
+						<h3>SAGE</h3>
+						<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque, quidem. Laudantium, autem?</p>
+						<button>More</button>
+					</li>
+				</ul>
+				<div>
+					<h3>CLIENTS</h3>
+					<ul>
+						<li>
+							<div>
+								<span>span</span>
+								text
+							</div>
+							<span></span>
+						</li>
+						<li>
+							<div>
+								<span>span</span>
+								text
+							</div>
+							<span></span>
+						</li>
+						<li>
+							<div>
+								<span>span</span>
+								text
+							</div>
+							<span></span>
+						</li>
+						<li>
+							<div>
+								<span>span</span>
+								text
+							</div>
+							<span></span>
+						</li>
+						<li>
+							<div>
+								<span>span</span>
+								text
+							</div>
+							<span></span>
+						</li>
+						<li>
+							<div>
+								<span>span</span>
+								text
+							</div>
+							<span></span>
+						</li>
+						<li>
+							<div>
+								<span>span</span>
+								text
+							</div>
+							<span></span>
+						</li>
+					</ul>
+				</div>
+			</article>
 			<article className='department-bottom'></article>
 		</Layout>
 	);

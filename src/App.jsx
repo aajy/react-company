9182238489e877926e95dd727ed7ee0bf2f2d0b4
aajy <aajy.000@gmail.com>
2023-12-03 22:04:57 +0900

    sub 페이지 layout 태그 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import Footer from './components/common/footer/Footer';
 import Header from './components/common/header/Header';
 import MainWrap from './components/main/mainWrap/MainWrap';
-import Members from './components/sub/members/Members';
 import Community from './components/sub/community/Community';
 import Contact from './components/sub/contact/Contact';
 import Department from './components/sub/department/Department';
@@ -32,7 +31,6 @@ export default function App() {
 			<Route path='/youtube' component={Youtube} />
 			<Route path='/gallery' component={Gallery} />
 			<Route path='/community' component={Community} />
-			<Route path='/members' component={Members} />
 			<Route path='/contact' component={Contact} />
 			<Footer />
 			<AnimatePresence>
--- src/components/sub/members/Members.jsx
+++
@@ -1,5 +0,0 @@
-import './Members.scss';
-
-export default function Members() {
-	return <div className='Members'>Members</div>;
-}

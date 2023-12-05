0650f99026c2320ca47d35b0a80b6310f1363954
aajy <aajy.000@gmail.com>
2023-12-05 23:40:32 +0900

    haeader 수정

--- src/components/common/header/Header.jsx
+++ src/components/common/header/Header.jsx
@@ -1,9 +1,9 @@
 import { useEffect, useState } from 'react';
 import './Header.scss';
 import { Link, NavLink } from 'react-router-dom/cjs/react-router-dom.min';
-import { LuSearch, LuChevronDown,LuChevronUp  } from 'react-icons/lu';
+import { LuSearch, LuChevronDown, LuChevronUp } from 'react-icons/lu';
 
-export default function Header({setToggleMenu, ToggleMenu }) {
+export default function Header({ setToggleMenu, ToggleMenu }) {
 	const [Global, setGlobal] = useState('');
 	const [IsGlobalDropBoxOpen, setIsGlobalDropBoxOpen] = useState(false);
 
@@ -16,7 +16,7 @@ export default function Header({setToggleMenu, ToggleMenu }) {
 	const handleGlobalOpen = () => {
 		setIsGlobalDropBoxOpen(!IsGlobalDropBoxOpen);
 	};
-	const handleGlobal = (e,el) => {
+	const handleGlobal = (e, el) => {
 		e.preventDefault();
 		const newGlobal = el;
 		setGlobal(newGlobal);
@@ -28,43 +28,6 @@ export default function Header({setToggleMenu, ToggleMenu }) {
 				<h1>
 					<Link to='/'>Abbvie</Link>
 				</h1>
-				<div className='dropBox'>
-					<div
-						className='container'
-						onBlur={(e) => handleBlurGlobalContainer(e)}
-					>
-						<label className='dropdown-button'>
-							<button onClick={() => handleGlobalOpen()}>
-								{Global ? Global  : `GLOBAL IN`}
-								{IsGlobalDropBoxOpen? <LuChevronUp />:<LuChevronDown />}
-							</button>
-						</label>
-						{IsGlobalDropBoxOpen && (
-							<div
-								className={
-									IsGlobalDropBoxOpen
-										? 'dropdown-container open'
-										: 'dropdown-container'
-								}
-							>
-								<ul className='dropdown-content'>
-									{globalList.map((el, idx) => (
-										<li key={idx} onClick={(e) => handleGlobal(e,el)}>
-											{el}
-										</li>
-									))}
-								</ul>
-							</div>
-						)}
-					</div>
-				</div>
-			</div>
-			<div className='bottom'>
-				<ul className='scrollMenu'>
-					<li>INVESTORS</li>
-					<li>NEWSCENTER</li>
-					<li>ASSISTANCE</li>
-				</ul>
 				<ul className='menu'>
 					<li>
 						<NavLink to='/department' activeClassName={'on'}>
@@ -91,9 +54,6 @@ export default function Header({setToggleMenu, ToggleMenu }) {
 							Contact
 						</NavLink>
 					</li>
-					<button>
-						<LuSearch />
-					</button>
 				</ul>
 				<button
 					className='menuToggle'
@@ -101,6 +61,53 @@ export default function Header({setToggleMenu, ToggleMenu }) {
 				>
 					menu
 				</button>
+			</div>
+			<div className='bottom'>
+				<ul className='scrollMenu'>
+					<li>How to</li>
+					<li>Careers</li>
+					<li>Blog</li>
+				</ul>
+				<div className='buttons'>
+					<span>
+						<input type='text' placeholder='Try searching for...' />
+						<button>
+							<LuSearch />
+						</button>
+					</span>
+					<button>Sign in</button>
+					<button>Sign up</button>
+				</div>
+				<div className='dropBox'>
+					<div
+						className='container'
+						onBlur={(e) => handleBlurGlobalContainer(e)}
+					>
+						<label className='dropdown-button'>
+							<button onClick={() => handleGlobalOpen()}>
+								{Global ? Global : `GLOBAL IN`}
+								{IsGlobalDropBoxOpen ? <LuChevronUp /> : <LuChevronDown />}
+							</button>
+						</label>
+						{IsGlobalDropBoxOpen && (
+							<div
+								className={
+									IsGlobalDropBoxOpen
+										? 'dropdown-container open'
+										: 'dropdown-container'
+								}
+							>
+								<ul className='dropdown-content'>
+									{globalList.map((el, idx) => (
+										<li key={idx} onClick={(e) => handleGlobal(e, el)}>
+											{el}
+										</li>
+									))}
+								</ul>
+							</div>
+						)}
+					</div>
+				</div>
 			</div>
 		</header>
 	);

87de4a9faaeac5f5ccfb2816cb1bb07ffd0bf4bf
aajy <aajy.000@gmail.com>
2023-12-12 01:33:18 +0900

    youtube detail 페이지 더보기 토글버튼, 스타일링 추가

--- src/components/sub/youtube/Detail.jsx
+++ src/components/sub/youtube/Detail.jsx
@@ -2,7 +2,10 @@ import { useParams } from 'react-router-dom';
 import Layout from '../../common/layout/Layout';
 import './Detail.scss';
 import { useEffect, useRef, useState } from 'react';
-import { IoMdHeart } from "react-icons/io";
+import { IoMdHeart } from 'react-icons/io';
+import { LiaComment } from 'react-icons/lia';
+import { useCustomText } from '../../../hooks/useText';
+import { TfiPlus, TfiMinus } from 'react-icons/tfi';
 
 export default function Detail() {
 	const path = useRef(process.env.PUBLIC_URL);
@@ -10,76 +13,118 @@ export default function Detail() {
 	const [YoutubeData, setYoutubeData] = useState(null);
 	const [CommentData, setCommentData] = useState([]);
 	const [IsComment, setIsComment] = useState(false);
+	const [Ismore, setIsmore] = useState(false);
+
+	const shortenText = useCustomText('shorten');
 
 	const fetchSingledata = async () => {
 		const api_key = process.env.REACT_APP_YOUTUBE_API;
 		const baseURL = `https://www.googleapis.com/youtube/v3/playlistItems?key=${api_key}&part=snippet&id=${id}`;
-    
 
-    try {
-      const data = await fetch(baseURL);
-      const json = await data.json();
+		try {
+			const data = await fetch(baseURL);
+			const json = await data.json();
 			console.log('json.items[0]: ', json.items[0]);
-      setYoutubeData(json.items[0].snippet);
-			//댓글가져오는 요청 
-      const commentURL = `https://www.googleapis.com/youtube/v3/commentThreads?key=${api_key}&part=replies&videoId=${json.items[0].snippet.resourceId.videoId}`
-      
-      try {
-        const data = await fetch(commentURL);
-        const json = await data.json();
-        console.log('comment------',json.items[0].replies.comments);
-				if (json.items[0].replies.comments.length > 0){
+			setYoutubeData(json.items[0].snippet);
+			//댓글가져오는 요청
+			const commentURL = `https://www.googleapis.com/youtube/v3/commentThreads?key=${api_key}&part=replies&videoId=${json.items[0].snippet.resourceId.videoId}`;
+
+			try {
+				const data = await fetch(commentURL);
+				const json = await data.json();
+				console.log('comment------', json.items[0].replies.comments);
+				if (json.items[0].replies.comments.length > 0) {
 					setIsComment(true);
 					setCommentData(json.items[0].replies.comments);
 				} else {
 					setIsComment(false);
 				}
-      }  catch(err) {
-        console.log(err);
-      }
-    } catch(err) {
-      console.log(err);
-    }
+			} catch (err) {
+				console.log(err);
+			}
+		} catch (err) {
+			console.log(err);
+		}
 	};
 	useEffect(() => {
 		fetchSingledata();
 	}, []);
 	return (
 		<Layout title={'Detail'} className={'Detail'}>
-		{(YoutubeData&& CommentData) && (
-			<article>
-				<div className='videoBox'>
-					<iframe
-						title={YoutubeData.title}
-						src={`https://www.youtube.com/embed/${YoutubeData.resourceId.videoId}`}
-						frameborder='0'
-						allowfullscreen="allowfullscreen"
-					></iframe>
-				</div>
-				<h3>{YoutubeData.title}</h3>
-				<p>{YoutubeData.description}</p>
-				<span>댓글 {CommentData.length} 개</span>
-				{IsComment && CommentData.map((comment,idx)=>{
-					return(
-						<div className='comment' key={comment.id + idx}>
-							<div className='profile'>
-								<div>
-									<img src={comment.snippet.authorProfileImageUrl} alt="profileUrl" />
-									<div>
+			{YoutubeData && CommentData && (
+				<article>
+					<div className='videoBox'>
+						<iframe
+							title={YoutubeData.title}
+							src={`https://www.youtube.com/embed/${YoutubeData.resourceId.videoId}`}
+							frameborder='0'
+							allowfullscreen='allowfullscreen'
+						></iframe>
+					</div>
+					<h3>{YoutubeData.title}</h3>
+					<p>
+						{YoutubeData.description.length > 300 ? (
+							<p>
+								{Ismore ? (
+									<span>
+										{YoutubeData.description}
+										<strong onClick={() => setIsmore(!Ismore)}>
+											briefly
+											<TfiMinus />
+										</strong>
+									</span>
+								) : (
+									<span>
+										{shortenText(YoutubeData.description, 300)}
+										<strong onClick={() => setIsmore(!Ismore)}>
+											see more
+											<TfiPlus />
+										</strong>
+									</span>
+								)}
+							</p>
+						) : (
+							<span>
+								{YoutubeData.description ||
+									`Amet consectetur adipisicing elit.
+								Ex harum temporibus laboriosam esse accusamus ea voluptates cum
+								amet, omnis, eius aliquid nihil, consectetur quia. Dolor
+								perferendis quidem illum nesciunt architecto.`}
+							</span>
+						)}
+					</p>
+					<span>
+						<LiaComment />
+						댓글 {CommentData.length} 개
+					</span>
+					{IsComment &&
+						CommentData.map((comment, idx) => {
+							return (
+								<div className='comment' key={comment.id + idx}>
+									<div className='profile'>
+										<div>
+											<img
+												src={comment.snippet.authorProfileImageUrl}
+												alt='profileUrl'
+											/>
+											<div>
+												<p>
+													<span>{comment.snippet.authorDisplayName}</span>
+													<span>{comment.snippet.publishedAt}</span>
+												</p>
+												<p>{comment.snippet.textDisplay}</p>
+											</div>
+										</div>
 										<p>
-											<span>{comment.snippet.authorDisplayName}</span>
-											<span>{comment.snippet.publishedAt}</span>
+											<IoMdHeart />
+											{comment.snippet.likeCount}
 										</p>
-										<p>{comment.snippet.textDisplay}</p>
 									</div>
 								</div>
-								<p><IoMdHeart />{comment.snippet.likeCount}</p>
-							</div>
-						</div>
-					)
-				})}
-			</article>
-		)}
-	</Layout>
+							);
+						})}
+				</article>
+			)}
+		</Layout>
 	);
 }

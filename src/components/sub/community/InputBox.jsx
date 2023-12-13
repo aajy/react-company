bffb9bf2bfec0c8d93d71412fe39fb969d2619ba
aajy <aajy.000@gmail.com>
2023-12-13 23:51:35 +0900

    InputBox 초기화, 등록 버튼 추가

--- src/components/sub/community/InputBox.jsx
+++ src/components/sub/community/InputBox.jsx
@@ -1,8 +1,15 @@
-import { useCallback, useRef } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import './InputBox.scss';
 import { GrPowerReset } from 'react-icons/gr';
 
 export default function InputBox() {
+	const getLocalData = () => {
+		const data = localStorage.getItem('post');
+		if (data) return JSON.parse(data);
+		else return [];
+	};
+	const [Post, setPost] = useState(getLocalData());
+
 	const refTit = useRef(null);
 	const refCon = useRef(null);
 	const inputRef = useRef(null);
@@ -14,11 +21,42 @@ export default function InputBox() {
 		}
 		console.log(e.target.files[0].name);
 	}, []);
-	const resetPost = (e) => {
-		e.preventDefault();
+	const resetPost = () => {
+		refTit.current.value = '';
+		refCon.current.value = '';
+		inputRef.current.value = '';
+		refNickname.current.value = '';
+		console.log(refNickname.current);
 	};
+
+	const createPost = () => {
+		if (!refTit.current.value.trim() || !refCon.current.value.trim()) {
+			resetPost();
+			return alert('제목과 본문을 모두 입력하세요.');
+		}
+		const korTime = new Date().getTime() + 1000 * 60 * 60 * 9;
+		setPost([
+			{
+				title: refTit.current.value,
+				content: refCon.current.value,
+				nickname: refNickname.current.value,
+				date: new Date(korTime),
+			},
+			...Post,
+		]);
+		resetPost();
+	};
+
+	useEffect(() => {
+		//Post데이터가 변경되면 수정모드를 강제로 false처리하면서 로컬저장소에 저장하고 컴포넌트 재실행
+		Post.map((el) => (el.enableUpdate = false));
+		localStorage.setItem('post', JSON.stringify(Post));
+		console.log('Post', Post);
+	}, [Post]);
 	return (
 		<div className='InputBox'>
+			<h2>Let's Talk</h2>
+			<p>Ask us anything or just say hi!</p>
 			<input type='text' placeholder='title' ref={refTit} />
 			<textarea
 				cols='30'
@@ -37,6 +75,7 @@ export default function InputBox() {
 				<button onClick={resetPost}>
 					<GrPowerReset />
 				</button>
+				<button onClick={createPost}>+</button>
 			</nav>
 		</div>
 	);

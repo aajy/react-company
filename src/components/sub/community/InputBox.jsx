6e2e2697da60ea2b6d8de30c7a0a6c94e9e51850
aajy <aajy.000@gmail.com>
2023-12-13 23:25:47 +0900

    Input.jsx jsx구성 title, textarea, nickname, img

--- src/components/sub/community/InputBox.jsx
+++ src/components/sub/community/InputBox.jsx
@@ -1,28 +1,43 @@
-import { useRef } from 'react';
+import { useCallback, useRef } from 'react';
 import './InputBox.scss';
 import { GrPowerReset } from 'react-icons/gr';
 
 export default function InputBox() {
 	const refTit = useRef(null);
 	const refCon = useRef(null);
+	const inputRef = useRef(null);
+	const refNickname = useRef(null);
+
+	const onUploadImage = useCallback((e) => {
+		if (!e.target.files) {
+			return;
+		}
+		console.log(e.target.files[0].name);
+	}, []);
+	const resetPost = (e) => {
+		e.preventDefault();
+	};
 	return (
 		<div className='InputBox'>
-			<div className='inputBox'>
-				<input type='text' placeholder='title' ref={refTit} />
-				<textarea
-					cols='30'
-					rows='3'
-					placeholder='content'
-					ref={refCon}
-				></textarea>
-
-				<nav>
-					{/* <button onClick={resetPost}> */}
-					<button>
-						<GrPowerReset />
-					</button>
-				</nav>
-			</div>
+			<input type='text' placeholder='title' ref={refTit} />
+			<textarea
+				cols='30'
+				rows='3'
+				placeholder='content'
+				ref={refCon}
+			></textarea>
+			<input type='text' placeholder='nickname' ref={refNickname} />
+			<input
+				type='file'
+				accept='image/jpg,image/jpeg,image/png,image/svg,image/gif'
+				ref={inputRef}
+				onChange={onUploadImage}
+			></input>
+			<nav>
+				<button onClick={resetPost}>
+					<GrPowerReset />
+				</button>
+			</nav>
 		</div>
 	);
 }

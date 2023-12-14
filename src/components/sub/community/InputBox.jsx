9fa03504bcbfc90c223249fd8d9669060698c375
aajy <aajy.000@gmail.com>
2023-12-15 01:48:49 +0900

    Community InputBox 컴포넌트 기본 스타일링

--- src/components/sub/community/InputBox.jsx
+++ src/components/sub/community/InputBox.jsx
@@ -2,32 +2,31 @@ import { useCallback, useEffect, useRef, useState } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 import './InputBox.scss';
 import { GrPowerReset } from 'react-icons/gr';
+import { TbCircleCheckFilled } from "react-icons/tb";
 
 export default function InputBox({ Open }) {
+	const path = useRef(process.env.PUBLIC_URL);
 	const getLocalData = () => {
 		const data = localStorage.getItem('post');
 		if (data) return JSON.parse(data);
 		else return [];
 	};
 	const [Post, setPost] = useState(getLocalData());
-
 	const refTit = useRef(null);
 	const refCon = useRef(null);
-	const inputRef = useRef(null);
 	const refNickname = useRef(null);
+	const refSelectedCharacter = useRef(0);
+	const [CharacterSrc, setCharacterSrc] = useState('');
+	const CharacterData = [
+		"character1","character2","character3","character4","character5"
+	]
 
-	const onUploadImage = useCallback((e) => {
-		if (!e.target.files) {
-			return;
-		}
-		console.log(e.target.files[0].name);
-	}, []);
 	const resetPost = () => {
 		refTit.current.value = '';
 		refCon.current.value = '';
-		inputRef.current.value = '';
+		refSelectedCharacter.current = 0;
+		setCharacterSrc('');
 		refNickname.current.value = '';
-		console.log(refNickname.current);
 	};
 
 	const createPost = () => {
@@ -42,22 +41,34 @@ export default function InputBox({ Open }) {
 				content: refCon.current.value,
 				nickname: refNickname.current.value,
 				date: new Date(korTime),
+				src:CharacterSrc
 			},
 			...Post,
 		]);
 		resetPost();
 	};
+	const handleCharacter = (e, idx) => {
+		const src = e.currentTarget.src;
+		setCharacterSrc(src);
+		refSelectedCharacter.current=idx;
+  };
 
 	useEffect(() => {
 		//Post데이터가 변경되면 수정모드를 강제로 false처리하면서 로컬저장소에 저장하고 컴포넌트 재실행
 		Post.map((el) => (el.enableUpdate = false));
 		localStorage.setItem('post', JSON.stringify(Post));
-		console.log('Post', Post);
 	}, [Post]);
 	return (
 		<AnimatePresence>
 			{Open && (
-				<motion.aside>
+				<motion.aside
+					initial={{ x: -100 }}
+					animate={{ x: 0, transition: { duration: 0.2, ease: 'linear' } }}
+					exit={{
+						x: -100,
+						opacity:0,
+						transition: { duration: 0.2, ease: 'linear'  },
+					}}>
 					<div className='InputBox'>
 						<h2>Let's Talk</h2>
 						<p>Ask us anything or just say hi!</p>
@@ -72,12 +83,19 @@ export default function InputBox({ Open }) {
 						></textarea>
 						<span>Selective Info</span>
 						<input type='text' placeholder='nickname' ref={refNickname} />
-						<input
-							type='file'
-							accept='image/jpg,image/jpeg,image/png,image/svg,image/gif'
-							ref={inputRef}
-							onChange={onUploadImage}
-						></input>
+						<div className="character">
+							<ul>
+								{CharacterData.map((img,idx)=>{
+									return (
+										<li key={img+idx}
+										className={refSelectedCharacter.current === idx ? 'on' : ''}>
+											<img src={`${path.current}/img/${img}.jpg`} alt="" onClick={(e)=>handleCharacter(e, idx)}/>
+											{refSelectedCharacter.current === idx && <span><TbCircleCheckFilled /></span>}
+										</li>
+									)
+								})}
+							</ul>
+						</div>
 						<nav>
 							<button onClick={resetPost}>
 								<GrPowerReset />

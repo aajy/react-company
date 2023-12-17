18c544683fb22ede916ce27af04c7898c6fe6fac
aajy <aajy.000@gmail.com>
2023-12-17 16:42:13 +0900

    Community inputBox  Modal css최종

--- src/components/sub/community/InputBox.jsx
+++ src/components/sub/community/InputBox.jsx
@@ -53,75 +53,77 @@ export default function InputBox({ Open, setNewPost }) {
 						animate={{ opacity:1 }}
 						exit={{ opacity:0 }}
 						className='modal'
-					></motion.div>
-					<motion.aside
-						initial={{ y: 0 }}
-						animate={{
-							y: 100,
-							transition: { duration: 0.2, ease: 'easeIn' },
-						}}
-						exit={{
-							y: 0,
-							transition: { duration: 0.2, ease: 'linear' },
-						}}
 					>
-						<div className='InputBox'>
-							<h2>Ask us anything or just say hi!</h2>
-							<span>Required Info</span>
-							<label htmlFor='refTit'>Title : </label>
-							<input type='text' placeholder='title' ref={refTit} id='refTit' />
-							<label htmlFor='refCon'>Content : </label>
-							<textarea
-								cols='30'
-								rows='3'
-								placeholder='content'
-								ref={refCon}
-								id='refCon'
-							></textarea>
-							<span>Selective Info</span>
-							<label htmlFor='refNickname'>Nickname : </label>
-							<input
-								type='text'
-								placeholder='nickname'
-								ref={refNickname}
-								id='refNickname'
-							/>
-							<span>choose your charater!</span>
-							<div className='theme'>
-								<ul>
-									<li>Theme :</li>
-									{ThemeData.map((img, idx) => {
-										return (
-											<li key={img + idx}>
-												{/* <img
-													src={`${path.current}/img/${img}.jpg`}
-													alt=''
-													onClick={(e) => handleTheme(e, img)}
-												/> */}
-												<span
-													className={img}
-													onClick={(e) => handleTheme(e, img)}
-												></span>
-												{ThemeName === img && (
-													<em>
-														<TbCircleCheckFilled />
-													</em>
-												)}
-											</li>
-										);
-									})}
-								</ul>
+						<motion.aside
+							initial={{ y: 0 }}
+							animate={{
+								y: 100,
+								transition: { duration: 0.2, ease: 'easeIn' },
+							}}
+							exit={{
+								y: 0,
+								transition: { duration: 0.2, ease: 'linear' },
+							}}
+						>
+							<div className='InputBox'>
+								<h2>Ask us anything or just say hi!</h2>
+								<span>Required Info</span>
+								<label htmlFor='refTit'>Title : </label>
+								<input type='text' placeholder='title' ref={refTit} id='refTit' />
+								<label htmlFor='refCon'>Content : </label>
+								<textarea
+									cols='30'
+									rows='3'
+									placeholder='content'
+									ref={refCon}
+									id='refCon'
+								></textarea>
+								<span>Selective Info</span>
+								<label htmlFor='refNickname'>Nickname : </label>
+								<input
+									type='text'
+									placeholder='nickname'
+									ref={refNickname}
+									id='refNickname'
+								/>
+								<span>choose your charater!</span>
+								<div className='theme'>
+									<ul>
+										<li>Theme :</li>
+										{ThemeData.map((img, idx) => {
+											return (
+												<li key={img + idx}>
+													{/* <img
+														src={`${path.current}/img/${img}.jpg`}
+														alt=''
+														onClick={(e) => handleTheme(e, img)}
+													/> */}
+													<span
+														className={img}
+														onClick={(e) => handleTheme(e, img)}
+													></span>
+													{ThemeName === img && (
+														<em>
+															<TbCircleCheckFilled />
+														</em>
+													)}
+												</li>
+											);
+										})}
+									</ul>
+								</div>
+								<nav>
+									<button onClick={resetInput}>
+										<GrPowerReset />
+									</button>
+									<button onClick={createPost}>
+										<BiCommentCheck />
+									</button>
+								</nav>
 							</div>
-							<nav>
-								<button onClick={resetInput}>
-									<GrPowerReset />
-								</button>
-								<button onClick={createPost}>
-									<BiCommentCheck />
-								</button>
-							</nav>
-						</div>
-					</motion.aside>
+						</motion.aside>
+					</motion.div>
+					
 				</>
 			)}
 		</AnimatePresence>

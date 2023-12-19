95f54ac1e3da1368ca9e3913dec5f94da9546be2
aajy <aajy.000@gmail.com>
2023-12-20 00:16:43 +0900

    reducer if else문으로 수정

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,32 +1,34 @@
 import { combineReducers } from 'redux';
+import * as types from './action';
 
 const memberReducer = (state = { members: [] }, action) => {
-	switch (action.type) {
-		case 'SET_MEMBERS':
-			return { ...state, members: action.payload };
-		default:
-			return state;
-	}
+	if (action.type === types.MEMBERS.start) return state;
+	else if (action.type === types.MEMBERS.success)
+		return { ...state, members: action.payload };
+	else if (action.type === types.MEMBERS.fail)
+		return { ...state, members: action.payload };
+	else return state;
 };
-const menuTextReducer = (state = { menuTextArr: [] }, action) => {
-	switch (action.type) {
-		case 'SET_MENUTEXT':
-			return { ...state, menuTextArr: action.payload };
-		default:
-			return state;
-	}
+const menuTextReducer = (state = { menuText: [] }, action) => {
+	if (action.type === types.MENUTEXT.start) return state;
+	else if (action.type === types.MENUTEXT.success)
+		return { ...state, menuText: action.payload };
+	else if (action.type === types.MENUTEXT.fail)
+		return { ...state, menuText: action.payload };
+	else return state;
 };
 const menuReducer = (state = { menu: false }, action) => {
-	switch (action.type) {
-		case 'SET_MENU':
-			return { ...state, menu: action.payload };
-		default:
-			return state;
-	}
+	if (action.type === types.MENU.start) return state;
+	else return state;
+};
+const darkReducer = (state = { dark: false }, action) => {
+	if (action.type === types.DARK.start) return state;
+	else return state;
 };
 const reducers = combineReducers({
 	memberReducer,
 	menuTextReducer,
 	menuReducer,
+	darkReducer,
 });
 export default reducers;

e11e95020b21b16f0e7aabe09a55ea36aac39735
aajy <aajy.000@gmail.com>
2023-12-29 11:30:09 +0900

    vercel branch react-query로 변경

--- src/components/sub/youtube/Detail.jsx
+++ src/components/sub/youtube/Detail.jsx
@@ -8,7 +8,7 @@ import { useCustomText } from '../../../hooks/useText';
 import { TfiPlus, TfiMinus } from 'react-icons/tfi';
 
 export default function Detail() {
-	const path = useRef(process.env.PUBLIC_URL);
+	// const path = useRef(process.env.PUBLIC_URL);
 	const { id } = useParams();
 	const [YoutubeData, setYoutubeData] = useState(null);
 	const [CommentData, setCommentData] = useState([]);
@@ -18,14 +18,16 @@ export default function Detail() {
 	const dummyData = [
 		{
 			snippet: {
-				authorProfileImageUrl : "https://yt3.ggpht.com/WggXR-cqxBtQN9zX9BpUX6gNTkn_zNjMAIzDe_n4SQP90MLrrqywFqWNSnljNuDWx9dQ_urf=s68-c-k-c0x00ffffff-no-rj",
-				authorDisplayName : "@maccosmetics",
-				publishedAt : "2023-12-06T19:45:13Z",
-				textDisplay : "Ex harum temporibus laboriosam esse accusamus ea voluptates cumamet, omnis, eius aliquid nihil, consectetur quia. Dolor perferendis quidem illum nesciunt",
-				likeCount : 10
-			}
-		}
-	]
+				authorProfileImageUrl:
+					'https://yt3.ggpht.com/WggXR-cqxBtQN9zX9BpUX6gNTkn_zNjMAIzDe_n4SQP90MLrrqywFqWNSnljNuDWx9dQ_urf=s68-c-k-c0x00ffffff-no-rj',
+				authorDisplayName: '@maccosmetics',
+				publishedAt: '2023-12-06T19:45:13Z',
+				textDisplay:
+					'Ex harum temporibus laboriosam esse accusamus ea voluptates cumamet, omnis, eius aliquid nihil, consectetur quia. Dolor perferendis quidem illum nesciunt',
+				likeCount: 10,
+			},
+		},
+	];
 	const fetchSingledata = async () => {
 		const api_key = process.env.REACT_APP_YOUTUBE_API;
 		const baseURL = `https://www.googleapis.com/youtube/v3/playlistItems?key=${api_key}&part=snippet&id=${id}`;
@@ -103,32 +105,31 @@ export default function Detail() {
 						<LiaComment />
 						댓글 {CommentData.length} 개
 					</span>
-						{CommentData.map((comment, idx) => {
-							return (
-								<div className='comment' key={comment.id + idx}>
-									<div className='profile'>
+					{CommentData.map((comment, idx) => {
+						return (
+							<div className='comment' key={comment.id + idx}>
+								<div className='profile'>
+									<div>
+										<img
+											src={comment.snippet.authorProfileImageUrl}
+											alt='profileUrl'
+										/>
 										<div>
-											<img
-												src={comment.snippet.authorProfileImageUrl}
-												alt='profileUrl'
-											/>
-											<div>
-												<p>
-													<span>{comment.snippet.authorDisplayName}</span>
-													<span>{comment.snippet.publishedAt}</span>
-												</p>
-												<p>{comment.snippet.textDisplay}</p>
-											</div>
+											<p>
+												<span>{comment.snippet.authorDisplayName}</span>
+												<span>{comment.snippet.publishedAt}</span>
+											</p>
+											<p>{comment.snippet.textDisplay}</p>
 										</div>
-										<p>
-											<IoMdHeart />
-											{comment.snippet.likeCount}
-										</p>
 									</div>
+									<p>
+										<IoMdHeart />
+										{comment.snippet.likeCount}
+									</p>
 								</div>
-							);
-						})
-					}
+							</div>
+						);
+					})}
 				</article>
 			)}
 		</Layout>

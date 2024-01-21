e3b16f6f857b9110cd166bdaabbf16e78f1be2db
aajy <aajy.000@gmail.com>
2024-01-21 22:31:56 +0900

    Banner 반응형 스타일링

--- src/components/main/banner/Banner.jsx
+++ src/components/main/banner/Banner.jsx
@@ -4,11 +4,29 @@ import { LuMonitorPlay } from "react-icons/lu";
 import { RiArrowRightUpLine } from 'react-icons/ri';
 import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 import { useCustomText } from '../../../hooks/useText';
+import { useScroll } from '../../../hooks/useScroll';
 
 export default function Banner(){
   const refBox = useRef(null);
   const [Vids, setVids] = useState([]);
   const shortenText = useCustomText('shorten');
+  const titEl = useRef(null);
+	const titEl2 = useRef(null);
+
+	const handleCustomScroll = scroll => {
+		if (scroll < 0) {
+			titEl.current.style.transform = `scale(3) translateX(${scroll}px)`;
+			titEl.current.style.opacity = 0;
+			titEl2.current.style.transform = `translateX(${scroll * 0.5}px)`;
+		} else {
+			titEl.current.style.transform = `scale(1) translateX(0px)`;
+			titEl.current.style.opacity = 1;
+			titEl2.current.style.transform = `translateX(0px)`;
+			titEl2.current.style.opacity = 1;
+		}
+	};
+
+	const { refEl } = useScroll(handleCustomScroll);
   const fetchYoutube = async () => {
 		const api_key = process.env.REACT_APP_YOUTUBE_API;
 		const pid = process.env.REACT_APP_YOUTUBE_LIST;
@@ -39,11 +57,11 @@ export default function Banner(){
 		fetchYoutube();
 	}, []);
   return(
-    <div className='Banner'>
+    <div className='Banner' ref={refEl}>
       <section className="top">
         <div className='center'>
-          <h2>Featured Cases</h2>
-          <h3>2018 - 2023</h3>
+          <h2 ref={titEl}>Featured Cases</h2>
+          <h3 ref={titEl2}>2018 - 2023</h3>
           <p><span><LuMonitorPlay/></span>From healthcare to education,<br/>eCommerce to travel, out innovative<br/>solutions are redefining the way<br/>businesses and individuals operate.</p>
         </div>
         <div className="right">
